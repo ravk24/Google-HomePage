b64eabff34e561b60d4c853727ecb6f955004ecf
ravk24 <ravk2493w@gmail.com>
2024-05-28 20:07:05 +0530

    changes by kushagra
    changes by kushagra

--- src/app/page.js
+++ src/app/page.js
@@ -2,21 +2,30 @@ import Image from "next/image";
 import HomeHeader from "./components/HomeHeader";
 import google_img from "../../public/images/google.svg";
 import HomeSearch from "./components/HomeSearch";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <div>
-      <HomeHeader />
-      <div className="flex flex-col items-center mt-24">
-        <Image
-          src={google_img}
-          alt="google.com"
-          width={300}
-          height={100}
-          priority
-        />
-        <HomeSearch />
-      </div>
+      <Suspense
+        fallback={
+          <div>
+            <p>Loading....</p>
+          </div>
+        }
+      >
+        <HomeHeader />
+        <div className="flex flex-col items-center mt-24">
+          <Image
+            src={google_img}
+            alt="google.com"
+            width={300}
+            height={100}
+            priority
+          />
+          <HomeSearch />
+        </div>
+      </Suspense>
     </div>
   );
 }
